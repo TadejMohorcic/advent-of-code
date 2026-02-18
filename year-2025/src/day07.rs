697c3d800aa70fff08159df1455eff74cb38bcc0
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-02-18 10:37:30 +0100

    day 07

--- year-2025/src/day07.rs
+++ year-2025/src/day07.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, BufReader, BufRead, Error};
+use std::io::{BufReader, BufRead, Error};
 
 pub fn main() -> Result<(), Error> {
     // let path = "input/day07_test.txt";
@@ -8,13 +8,19 @@ pub fn main() -> Result<(), Error> {
     let input = File::open(path)?;
     let buffered = BufReader::new(input);
 
+    let mut splitter_locations = Vec::new();
+
     for line in buffered.lines() {
         let line_ok = line?;
-        println!("{:?}", line_ok);
+
+        let splitter_row: Vec<u64> = line_ok.trim().chars().enumerate().filter_map(|(i, x)| (x == '^').then_some(i as u64)).collect();
+
+        if !splitter_row.is_empty() {
+            splitter_locations.push(splitter_row);
+        }
     }
 
-    let part_one = 0;
-    let part_two = 0;
+    let (part_one, part_two) = move_in_manifold(&splitter_locations);
 
     println!("--- Day 7: Laboratories ---");
     println!(" - Part one solution: {}", part_one);
@@ -22,4 +28,37 @@ pub fn main() -> Result<(), Error> {
     println!("");
 
     Ok(())
+}
+
+fn move_in_manifold(locations: &Vec<Vec<u64>>) -> (u64, u64) {
+    let mut number_of_splits = 0;
+
+    let starting_position = locations[0][0];
+
+    let mut beams = vec![0; 2 * (starting_position as usize) + 1];
+    beams[starting_position as usize] = 1;
+
+    for i in 0..locations.len() {
+        let mut new_beams = vec![0; 2 * (starting_position as usize) + 1];
+
+        let beam_ids: Vec<usize> = beams.iter().enumerate().filter_map(|(i, x)| (*x > 0).then_some(i)).collect();
+
+        for b in beam_ids {
+            if locations[i].contains(&(b as u64)) {
+                new_beams[b - 1] += beams[b];
+                new_beams[b + 1] += beams[b];
+                new_beams[b] = 0;
+                number_of_splits += 1;
+            }
+            else {
+                new_beams[b] += beams[b];
+            }
+        }
+
+        beams = new_beams;
+    }
+
+    let total_timelines = beams.iter().fold(0, |acc, x| acc + x);
+
+    (number_of_splits, total_timelines)
 }
--- year-2025/src/main.rs
+++ year-2025/src/main.rs
@@ -4,7 +4,7 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
-// mod day07;
+mod day07;
 // mod day08;
 // mod day09;
 // mod day10;
@@ -18,7 +18,7 @@ fn main() {
     let _ = day04::main();
     let _ = day05::main();
     let _ = day06::main();
-    // let _ = day07::main();
+    let _ = day07::main();
     // let _ = day08::main();
     // let _ = day09::main();
     // let _ = day10::main();
