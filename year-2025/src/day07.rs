6dc6923ab54aac8b81e3c96cd6550dad9762a0fc
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-16 08:52:35 +0200

    cleanup and tests

--- year-2025/src/day07.rs
+++ year-2025/src/day07.rs
@@ -1,50 +1,43 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::path::Path;
 
-pub fn main() -> Result<(), Error> {
-    let path = "input/day07.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut splitter_locations = Vec::new();
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-
-        let splitter_row: Vec<u64> = line_ok
-            .trim()
-            .chars()
-            .enumerate()
-            .filter_map(|(i, x)| (x == '^').then_some(i as u64))
-            .collect();
-
-        if !splitter_row.is_empty() {
-            splitter_locations.push(splitter_row);
-        }
-    }
-
-    let (part_one, part_two) = move_in_manifold(&splitter_locations);
+pub fn main() {
+    let splitters = parse_input("input/day07.txt");
+    let (part_one, part_two) = move_down_the_manifold(&splitters);
 
     println!("--- Day 7: Laboratories ---");
     println!(" - Part one solution: {}", part_one);
     println!(" - Part two solution: {}", part_two);
     println!("");
-
-    Ok(())
 }
 
-fn move_in_manifold(locations: &[Vec<u64>]) -> (u64, u64) {
-    let mut number_of_splits = 0;
+fn parse_input<P: AsRef<Path>>(filename: P) -> Vec<Vec<i64>> {
+    let mut splitters = Vec::new();
+    if let Ok(lines) = crate::read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            let row: Vec<i64> = line
+                .trim()
+                .chars()
+                .enumerate()
+                .filter_map(|(i, x)| (x == '^').then_some(i as i64))
+                .collect();
 
-    let starting_position = locations[0][0];
+            if !row.is_empty() {
+                splitters.push(row);
+            }
+        }
+    }
 
-    let mut beams = vec![0; 2 * (starting_position as usize) + 1];
-    beams[starting_position as usize] = 1;
+    splitters
+}
+
+fn move_down_the_manifold(locations: &[Vec<i64>]) -> (i64, i64) {
+    let mut splits = 0;
+    let starting_pos = locations[0][0];
+    let mut beams = vec![0; 2 * (starting_pos as usize) + 1];
+    beams[starting_pos as usize] = 1;
 
     for i in 0..locations.len() {
-        let mut new_beams = vec![0; 2 * (starting_position as usize) + 1];
-
+        let mut new_beams = vec![0; 2 * (starting_pos as usize) + 1];
         let beam_ids: Vec<usize> = beams
             .iter()
             .enumerate()
@@ -52,11 +45,11 @@ fn move_in_manifold(locations: &[Vec<u64>]) -> (u64, u64) {
             .collect();
 
         for b in beam_ids {
-            if locations[i].contains(&(b as u64)) {
+            if locations[i].contains(&(b as i64)) {
                 new_beams[b - 1] += beams[b];
                 new_beams[b + 1] += beams[b];
                 new_beams[b] = 0;
-                number_of_splits += 1;
+                splits += 1;
             } else {
                 new_beams[b] += beams[b];
             }
@@ -65,7 +58,24 @@ fn move_in_manifold(locations: &[Vec<u64>]) -> (u64, u64) {
         beams = new_beams;
     }
 
-    let total_timelines = beams.iter().fold(0, |acc, x| acc + x);
+    (splits, beams.iter().fold(0, |acc, x| acc + x))
+}
 
-    (number_of_splits, total_timelines)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let splitters = parse_input("input/day07-test.txt");
+        let (part_one, _) = move_down_the_manifold(&splitters);
+        assert_eq!(part_one, 21);
+    }
+
+    #[test]
+    fn part_two_example() {
+        let splitters = parse_input("input/day07-test.txt");
+        let (_, part_two) = move_down_the_manifold(&splitters);
+        assert_eq!(part_two, 40);
+    }
 }
