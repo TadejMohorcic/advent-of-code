0847b9de3981804b560657d7de7df4290992f4bb
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-20 20:59:12 +0200

    rewritten the logic, added tests, and linted with clippy

--- year-2025/src/day10.rs
+++ year-2025/src/day10.rs
@@ -1,207 +1,213 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
-
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
-
 use rayon::prelude::*;
+use rustc_hash::FxHashMap;
+use std::path::Path;
 
-pub fn main() -> Result<(), Error> {
-    let path = "input/day10.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut manuals = Vec::new();
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-        let mut split_line = line_ok.trim().split_whitespace();
-
-        let indicator_lights: Vec<i64> = split_line
-            .next()
-            .unwrap()
-            .trim_matches(|c| c == '[' || c == ']')
-            .chars()
-            .map(|c| match c {
-                '#' => 1,
-                _ => 0,
-            })
-            .collect();
-        let joltage_requirements: Vec<i64> = split_line
-            .next_back()
-            .unwrap()
-            .trim_matches(|c| c == '{' || c == '}')
-            .split(',')
-            .map(|c| c.parse().unwrap())
-            .collect();
-        let buttons: Vec<Vec<usize>> = split_line
-            .map(|c| {
-                c.trim_matches(|x| x == '(' || x == ')')
-                    .split(',')
-                    .map(|x| x.parse().unwrap())
-                    .collect()
-            })
-            .collect();
-
-        let manual = Manual {
-            indicator_lights: indicator_lights,
-            buttons: buttons,
-            joltage_requirements: joltage_requirements,
-        };
-        manuals.push(manual);
-    }
-
-    let part_one = min_button_presses(&manuals);
-    let part_two = configure_joltages(&manuals);
+pub fn main() {
+    let manuals = parse_input("input/day10.txt");
+    let part_one = toggle_lights(&manuals);
+    let part_two = toggle_joltages(&manuals);
 
     println!("--- Day 10: Factory ---");
     println!(" - Part one solution: {}", part_one);
-    println!(" - Part two solution: {}", part_two);
-    println!("");
-
-    Ok(())
+    println!(" - Part two solution: {}\n", part_two);
 }
 
 struct Manual {
-    indicator_lights: Vec<i64>,
+    indicator_light_diagram: Vec<i64>,
     buttons: Vec<Vec<usize>>,
     joltage_requirements: Vec<i64>,
 }
 
-fn get_combinations(buttons: &[Vec<usize>]) -> Vec<Vec<Vec<usize>>> {
-    let n = buttons.len();
+fn parse_input<P: AsRef<Path>>(filename: P) -> Vec<Manual> {
+    let mut manuals = Vec::new();
+
+    if let Ok(lines) = crate::read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            let mut line = line.split_whitespace();
+            let indicator_light_diagram = line
+                .next()
+                .unwrap()
+                .trim_matches(['[', ']'])
+                .chars()
+                .map(|c| match c {
+                    '#' => 1,
+                    _ => 0,
+                })
+                .collect();
+            let joltage_requirements = line
+                .next_back()
+                .unwrap()
+                .trim_matches(['{', '}'])
+                .split(',')
+                .map(|x| x.parse().unwrap())
+                .collect();
+            let buttons = line
+                .map(|c| {
+                    c.trim_matches(['(', ')'])
+                        .split(',')
+                        .map(|x| x.parse().unwrap())
+                        .collect()
+                })
+                .collect();
+            manuals.push(Manual {
+                indicator_light_diagram,
+                joltage_requirements,
+                buttons,
+            });
+        }
+    }
+
+    manuals
+}
+
+fn get_all_combinations(n: usize) -> Vec<Vec<usize>> {
+    let mut all_combinations = Vec::new();
     let total = 1 << n;
-    let mut power_set = Vec::new();
 
     for i in 0..total {
         let mut combination = Vec::new();
 
         for j in 0..n {
             if (i >> j) & 1 == 1 {
-                combination.push(buttons[j].clone());
+                combination.push(j);
             }
         }
 
-        power_set.push(combination);
+        all_combinations.push(combination);
     }
 
-    power_set
+    all_combinations
 }
 
-fn find_good_combinations<'a>(
+fn get_valid_combinations(
     lights: &[i64],
-    combinations: &'a [Vec<Vec<usize>>],
-) -> Vec<&'a Vec<Vec<usize>>> {
-    let n = lights.len();
-
-    let mut good_combinations = Vec::new();
+    buttons: &[Vec<usize>],
+    combinations: &[Vec<usize>],
+) -> Vec<Vec<usize>> {
+    let mut valid_combinations = Vec::new();
+    let m = lights.len();
 
     for combination in combinations {
         if combination.is_empty() {
             continue;
         }
 
-        let mut new_lights = vec![0; n];
+        let mut test_lights = vec![0; m];
 
-        for button in combination {
-            for position in button {
-                new_lights[*position] = (new_lights[*position] + 1) % 2;
+        for button_id in combination {
+            let button = &buttons[*button_id];
+
+            for toggle in button {
+                test_lights[*toggle] = (test_lights[*toggle] + 1) % 2;
             }
         }
 
-        if new_lights == *lights {
-            good_combinations.push(combination);
+        if test_lights == *lights {
+            valid_combinations.push(combination.clone());
         }
     }
 
-    good_combinations
+    valid_combinations
 }
 
-fn min_button_presses(manuals: &[Manual]) -> usize {
+fn toggle_lights(manuals: &[Manual]) -> usize {
     manuals
         .par_iter()
         .map(|manual| {
+            let lights = &manual.indicator_light_diagram;
             let buttons = &manual.buttons;
-            let lights = &manual.indicator_lights;
-            let combinations = get_combinations(buttons);
-            let good_combinations = find_good_combinations(lights, &combinations);
-            let mut combinations_len: Vec<usize> =
-                good_combinations.iter().map(|x| x.len()).collect();
-            combinations_len.sort();
+            let all_combinations = get_all_combinations(buttons.len());
+            let valid_combinations = get_valid_combinations(lights, buttons, &all_combinations);
+            let mut combination_lengths: Vec<usize> =
+                valid_combinations.iter().map(|combo| combo.len()).collect();
+            combination_lengths.sort();
 
-            combinations_len[0]
+            combination_lengths[0]
         })
         .sum()
 }
 
-fn recursive_joltage<'a>(
-    joltages: &Vec<i64>,
-    combinations: &'a [Vec<Vec<usize>>],
-    cache: &mut HashMap<Vec<i64>, Vec<&'a Vec<Vec<usize>>>>,
-) -> i64 {
-    let lights: Vec<i64> = joltages.iter().map(|x| x % 2).collect();
+fn configure_joltages_recursive(
+    joltages: &[i64],
+    buttons: &[Vec<usize>],
+    combinations: &[Vec<usize>],
+    cache: &mut FxHashMap<Vec<i64>, Vec<Vec<usize>>>,
+) -> usize {
+    let lights: Vec<i64> = joltages.iter().map(|n| n % 2).collect();
 
-    if lights.iter().all(|x| *x == 0) {
-        let new_joltages: Vec<i64> = joltages.iter().map(|x| x / 2).collect();
-
-        return 2 * recursive_joltage(&new_joltages, combinations, cache);
+    if lights.iter().all(|n| *n == 0) {
+        let new_joltages: Vec<i64> = joltages.iter().map(|n| n / 2).collect();
+        return 2 * configure_joltages_recursive(&new_joltages, buttons, combinations, cache);
     }
 
-    let good_combinations = match cache.entry(lights) {
-        Entry::Occupied(e) => e.get().clone(),
-        Entry::Vacant(e) => {
-            let computed = find_good_combinations(e.key(), combinations);
-            e.insert(computed).clone()
-        }
-    };
+    let good_combinations = cache
+        .entry(lights)
+        .or_insert_with_key(|lights| get_valid_combinations(lights, buttons, combinations))
+        .clone();
 
     if good_combinations.is_empty() {
         return 10000;
     }
 
-    let mut combinations_to_consider = Vec::new();
+    let mut combinations_length = Vec::new();
 
     for combination in good_combinations {
-        let button_presses: i64;
-        let mut copied_joltages = joltages.clone();
+        let mut joltages_copy = joltages.to_owned();
 
-        for button in combination {
-            for position in button {
-                copied_joltages[*position] -= 1;
+        for button_id in &combination {
+            let button = &buttons[*button_id];
+
+            for toggle in button {
+                joltages_copy[*toggle] -= 1;
             }
         }
 
-        let new_joltages: Vec<i64> = copied_joltages.iter().map(|x| x / 2).collect();
+        let new_joltages: Vec<i64> = joltages_copy.iter().map(|n| n / 2).collect();
 
-        if new_joltages.iter().all(|x| *x == 0) {
-            button_presses = combination.len() as i64;
-        } else if new_joltages.iter().any(|x| *x < 0) {
-            button_presses = 10000;
+        if new_joltages.iter().all(|n| *n == 0) {
+            combinations_length.push(combination.len());
+        } else if new_joltages.iter().any(|n| *n < 0) {
+            combinations_length.push(10000);
         } else {
-            button_presses = 2 * recursive_joltage(&new_joltages, combinations, cache)
-                + combination.len() as i64;
+            combinations_length.push(
+                2 * configure_joltages_recursive(&new_joltages, buttons, combinations, cache)
+                    + combination.len(),
+            )
         }
-
-        combinations_to_consider.push(button_presses);
     }
 
-    combinations_to_consider.sort();
+    combinations_length.sort();
 
-    combinations_to_consider[0]
+    combinations_length[0]
 }
 
-fn configure_joltages(manuals: &[Manual]) -> i64 {
+fn toggle_joltages(manuals: &[Manual]) -> usize {
     manuals
         .par_iter()
         .map(|manual| {
             let buttons = &manual.buttons;
             let joltages = &manual.joltage_requirements;
-            let combinations = get_combinations(buttons);
-            let mut cache = HashMap::new();
-            let button_presses = recursive_joltage(joltages, &combinations, &mut cache);
+            let all_combinations = get_all_combinations(buttons.len());
+            let mut cache = FxHashMap::default();
 
-            button_presses
+            configure_joltages_recursive(joltages, buttons, &all_combinations, &mut cache)
         })
         .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let manuals = parse_input("input/day10-test.txt");
+        assert_eq!(toggle_lights(&manuals), 7)
+    }
+
+    #[test]
+    fn part_two_example() {
+        let manuals = parse_input("input/day10-test.txt");
+        assert_eq!(toggle_joltages(&manuals), 33)
+    }
 }
