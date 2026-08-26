7044dbb181c9f4ba4e8a2bb58802e132cb61fe41
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-26 20:00:40 +0200

    updated main

--- year-2024/src/main.rs
+++ year-2024/src/main.rs
@@ -28,7 +28,7 @@ mod day24;
 fn main() {
     day01::main();
     day02::main();
-    let _ = day03::main();
+    day03::main();
     let _ = day05::main();
     let _ = day06::main();
     let _ = day07::main();
