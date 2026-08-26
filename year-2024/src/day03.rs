e984f43eadad8ac64e72578bbec1a14956be17f6
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-26 20:00:47 +0200

    refactor and tests

--- year-2024/src/day03.rs
+++ year-2024/src/day03.rs
@@ -1,62 +1,66 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
-
 use regex::Regex;
+use std::path::Path;
 
-pub fn main() -> Result<(), Error> {
-    // let path = "input/day03-test.txt";
-    let path = "input/day03.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut string_input = String::new();
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-        string_input.push_str(&line_ok);
-    }
-
-    let part_one = scan_for_multiplicators(
-        &string_input,
-        Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap(),
-    );
-    let part_two = scan_for_multiplicators(
-        &string_input,
-        Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap(),
-    );
+pub fn main() {
+    let memory = parse_input("input/day03.txt");
+    let instructions1 = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let instructions2 = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let part_one = multiply_instructions(&memory, instructions1);
+    let part_two = multiply_instructions(&memory, instructions2);
 
     println!("--- Day 3: Mull It Over ---");
     println!(" - Part one solution: {}", part_one);
-    println!(" - Part two solution: {}", part_two);
-    println!("");
-
-    Ok(())
+    println!(" - Part two solution: {}\n", part_two);
 }
 
-fn scan_for_multiplicators(string: &String, re: Regex) -> u64 {
+fn parse_input<P: AsRef<Path>>(filename: P) -> String {
+    let mut memory = String::new();
+
+    if let Ok(lines) = crate::read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            memory.push_str(line.trim());
+        }
+    }
+
+    memory
+}
+
+fn multiply_instructions(string: &str, re: Regex) -> i64 {
     let mut result = 0;
     let mut multiply = true;
 
     for capture in re.captures_iter(string) {
-        if let Some(x) = capture.get(1) {
-            let y = capture.get(2).unwrap();
+        match capture.get(1) {
+            Some(x) => {
+                if multiply {
+                    let x = x.as_str().parse::<i64>().unwrap();
+                    let y = capture.get(2).unwrap().as_str().parse::<i64>().unwrap();
 
-            if multiply {
-                let x_int: u64 = x.as_str().parse().unwrap();
-                let y_int: u64 = y.as_str().parse().unwrap();
-
-                result += x_int * y_int;
+                    result += x * y;
+                }
             }
-        } else {
-            let instruction = &capture[0];
-            multiply = if instruction == "don't()" {
-                false
-            } else {
-                true
-            };
+            None => multiply = &capture[0] != "don't()",
         }
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let memory = parse_input("input/day03-test1.txt");
+        let instructions = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+        assert_eq!(multiply_instructions(&memory, instructions), 161);
+    }
+
+    #[test]
+    fn part_two_example() {
+        let memory = parse_input("input/day03-test2.txt");
+        let instructions = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+        assert_eq!(multiply_instructions(&memory, instructions), 48);
+    }
 }
