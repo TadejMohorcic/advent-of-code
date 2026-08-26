38e2cf23f59c2a9bdf446f56c2d99a8657a00a61
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-26 20:48:19 +0200

    updated main

--- year-2024/src/main.rs
+++ year-2024/src/main.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 mod day05;
 mod day06;
 mod day07;
@@ -29,6 +30,7 @@ fn main() {
     day01::main();
     day02::main();
     day03::main();
+    day04::main();
     let _ = day05::main();
     let _ = day06::main();
     let _ = day07::main();
