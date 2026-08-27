41ef08d461cb5a0f8d9aa04ca1ab0b1e7f46aa8a
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-27 22:29:35 +0200

    refactored and added tests

--- year-2024/src/day07.rs
+++ year-2024/src/day07.rs
@@ -1,99 +1,96 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::path::Path;
 
-pub fn main() -> Result<(), Error> {
-    // let path = "input/day07-test.txt";
-    let path = "input/day07.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut equations = Vec::new();
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-
-        let (t, nums) = line_ok.trim().split_once(": ").unwrap();
-        let target: i64 = t.parse().unwrap();
-        let numbers: Vec<i64> = nums
-            .split_whitespace()
-            .map(|x| x.parse().unwrap())
-            .collect();
-
-        let eq = Equation {
-            target: target,
-            numbers: numbers,
-        };
-        equations.push(eq);
-    }
-
-    let part_one = calculate_calibration(&equations, false);
-    let part_two = calculate_calibration(&equations, true);
+pub fn main() {
+    let equations = parse_input("input/day07.txt");
+    let part_one = calibrate_equations(&equations, false);
+    let part_two = calibrate_equations(&equations, true);
 
     println!("--- Day 7: Bridge Repair ---");
     println!(" - Part one solution: {}", part_one);
-    println!(" - Part two solution: {}", part_two);
-    println!("");
-
-    Ok(())
+    println!(" - Part two solution: {}\n", part_two);
 }
 
-struct Equation {
+fn parse_input<P: AsRef<Path>>(filename: P) -> Vec<(i64, Vec<i64>)> {
+    let mut equations = Vec::new();
+
+    if let Ok(lines) = crate::read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            let split = line.trim().split_once(": ").unwrap();
+            let target = split.0.parse::<i64>().unwrap();
+            let numbers = split
+                .1
+                .split_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect();
+            equations.push((target, numbers))
+        }
+    }
+
+    equations
+}
+
+fn can_produce_target(
     target: i64,
-    numbers: Vec<i64>,
+    numbers: &[i64],
+    current_value: i64,
+    index: usize,
+    part: bool,
+) -> bool {
+    if index == numbers.len() {
+        return current_value == target;
+    } else if current_value > target {
+        return false;
+    }
+
+    let addition = can_produce_target(
+        target,
+        numbers,
+        current_value + numbers[index],
+        index + 1,
+        part,
+    );
+    let multiplication = can_produce_target(
+        target,
+        numbers,
+        current_value * numbers[index],
+        index + 1,
+        part,
+    );
+    let num_len = numbers[index].checked_ilog10().unwrap_or(0) + 1;
+    let concatenation = part
+        && can_produce_target(
+            target,
+            numbers,
+            current_value * 10_i64.pow(num_len) + numbers[index],
+            index + 1,
+            part,
+        );
+
+    addition || multiplication || concatenation
 }
 
-fn calculate_calibration(equations: &Vec<Equation>, part: bool) -> i64 {
+fn calibrate_equations(equations: &[(i64, Vec<i64>)], part: bool) -> i64 {
     equations
         .iter()
-        .map(|equation| {
-            let starting_value = equation.numbers[0];
-            if apply_operations(equation, starting_value, 1, part) {
-                equation.target
-            } else {
-                0
-            }
+        .filter_map(|(target, numbers)| {
+            can_produce_target(*target, numbers, numbers[0], 1, part).then_some(target)
         })
         .sum()
 }
 
-fn apply_operations(
-    equation: &Equation,
-    current_value: i64,
-    current_index: usize,
-    part: bool,
-) -> bool {
-    if current_index == equation.numbers.len() {
-        return current_value == equation.target;
-    } else if current_value > equation.target {
-        return false;
-    } else {
-        let next_number = equation.numbers[current_index];
-        let n = next_number.checked_ilog10().unwrap_or(0) + 1;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        let b1 = apply_operations(
-            equation,
-            current_value + next_number,
-            current_index + 1,
-            part,
-        );
-        let b2 = apply_operations(
-            equation,
-            current_value * next_number,
-            current_index + 1,
-            part,
-        );
-        let b3 = if part {
-            apply_operations(
-                equation,
-                current_value * 10_i64.pow(n) + next_number,
-                current_index + 1,
-                part,
-            )
-        } else {
-            false
-        };
+    #[test]
+    fn part_one_example() {
+        let equations = parse_input("input/day07-test.txt");
+        assert_eq!(calibrate_equations(&equations, false), 3749);
+    }
 
-        return b1 || b2 || b3;
+    #[test]
+    fn part_two_example() {
+        let equations = parse_input("input/day07-test.txt");
+        assert_eq!(calibrate_equations(&equations, true), 11387);
     }
 }
