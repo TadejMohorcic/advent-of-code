9c411077e8517d792038c5c0e21eb5078ca1224a
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-27 20:35:34 +0200

    refactored and added tests

--- year-2024/src/day05.rs
+++ year-2024/src/day05.rs
@@ -1,119 +1,86 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
-
 use std::collections::HashSet;
+use std::path::Path;
 
-pub fn main() -> Result<(), Error> {
-    // let path = "input/day05-test.txt";
-    let path = "input/day05.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut current_event = PageEvent::PageRules;
-
-    let mut page_orders = Vec::new();
-    let mut orderings = Vec::new();
-
-    let mut pages = HashSet::new();
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-
-        if line_ok.is_empty() {
-            current_event = PageEvent::PrintingOrder;
-            continue;
-        }
-
-        match current_event {
-            PageEvent::PageRules => {
-                let order: Vec<u64> = line_ok
-                    .trim()
-                    .split('|')
-                    .map(|x| x.parse().unwrap())
-                    .collect();
-
-                for page in &order {
-                    pages.insert(*page);
-                }
-
-                page_orders.push(order.clone());
-            }
-            PageEvent::PrintingOrder => {
-                let ordering: Vec<u64> = line_ok
-                    .trim()
-                    .split(',')
-                    .map(|x| x.parse().unwrap())
-                    .collect();
-                orderings.push(ordering);
-            }
-        }
-    }
-
-    let mut sorted_pages: Vec<u64> = pages.iter().map(|x| *x).collect();
-    sorted_pages.sort();
-
-    let (part_one, part_two) = check_order(&page_orders, &orderings, &sorted_pages);
+pub fn main() {
+    let (order_rules, pages) = parse_input("input/day05.txt");
+    let part_one = get_middle_page(&order_rules, &pages, false);
+    let part_two = get_middle_page(&order_rules, &pages, true);
 
     println!("--- Day 5: Print Queue ---");
     println!(" - Part one solution: {}", part_one);
-    println!(" - Part two solution: {}", part_two);
-    println!("");
-
-    Ok(())
+    println!(" - Part two solution: {}\n", part_two);
 }
 
-enum PageEvent {
-    PageRules,
-    PrintingOrder,
-}
+fn parse_input<P: AsRef<Path>>(filename: P) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
+    let mut page_order_rules = Vec::new();
+    let mut update_pages = Vec::new();
+    let mut is_rule = true;
 
-fn check_order(rules: &[Vec<u64>], pages: &[Vec<u64>], page_index: &Vec<u64>) -> (u64, u64) {
-    let mut part_one = 0;
-    let mut part_two = 0;
+    if let Ok(lines) = crate::read_lines(filename) {
+        for line in lines.map_while(Result::ok) {
+            if line.is_empty() {
+                is_rule = false;
+                continue;
+            }
 
-    let adjecency_matrix = build_adj_matrix(rules, page_index);
-
-    for page in pages {
-        if let Some(x) = is_order_valid(page, page_index, &adjecency_matrix) {
-            part_one += x;
-        } else {
-            part_two += bubble_sort(page, page_index, &adjecency_matrix);
+            if is_rule {
+                let rule = line
+                    .trim()
+                    .split('|')
+                    .map(|n| n.parse::<i64>().unwrap())
+                    .collect();
+                page_order_rules.push(rule);
+            } else {
+                let pages = line
+                    .trim()
+                    .split(',')
+                    .map(|n| n.parse::<i64>().unwrap())
+                    .collect();
+                update_pages.push(pages);
+            }
         }
     }
 
-    (part_one, part_two)
+    (page_order_rules, update_pages)
 }
 
-fn build_adj_matrix(rules: &[Vec<u64>], page_index: &Vec<u64>) -> Vec<Vec<u32>> {
-    let n = page_index.len();
+fn build_adjacency_matrix(order_rules: &[Vec<i64>]) -> (Vec<Vec<i64>>, Vec<i64>) {
+    let mut page_numbers = HashSet::new();
 
-    let mut matrix: Vec<Vec<u32>> = vec![vec![0; n]; n];
-
-    for rule in rules {
-        let before = page_index.iter().position(|n| *n == rule[0]).unwrap() as usize;
-        let after = page_index.iter().position(|n| *n == rule[1]).unwrap() as usize;
-
-        matrix[before][after] = 1;
+    for rule in order_rules {
+        page_numbers.insert(rule[0]);
+        page_numbers.insert(rule[1]);
     }
 
-    matrix
+    let mut sorted_pages: Vec<i64> = page_numbers.into_iter().collect();
+    sorted_pages.sort();
+    let n = sorted_pages.len();
+
+    let mut adjacency_matrix = vec![vec![0; n]; n];
+
+    for rule in order_rules {
+        let before = sorted_pages.iter().position(|n| *n == rule[0]).unwrap();
+        let after = sorted_pages.iter().position(|n| *n == rule[1]).unwrap();
+
+        adjacency_matrix[before][after] = 1;
+    }
+
+    (adjacency_matrix, sorted_pages)
 }
 
-fn is_order_valid(
-    page: &Vec<u64>,
-    page_index: &Vec<u64>,
-    adj_matrix: &Vec<Vec<u32>>,
-) -> Option<u64> {
+fn is_page_ordered(
+    page: &[i64],
+    adjacency_matrix: &[Vec<i64>],
+    sorted_pages: &[i64],
+) -> Option<i64> {
     let n = page.len();
 
     for i in 0..n - 1 {
-        let before = page_index.iter().position(|n| *n == page[i]).unwrap() as usize;
+        let before = sorted_pages.iter().position(|n| *n == page[i]).unwrap();
+        for p in page.iter().take(n).skip(i + 1) {
+            let after = sorted_pages.iter().position(|n| n == p).unwrap();
 
-        for j in i + 1..n {
-            let after = page_index.iter().position(|n| *n == page[j]).unwrap() as usize;
-
-            if adj_matrix[before][after] != 1 {
+            if adjacency_matrix[before][after] != 1 {
                 return None;
             }
         }
@@ -122,40 +89,75 @@ fn is_order_valid(
     Some(page[n / 2])
 }
 
-fn bubble_sort(page: &Vec<u64>, page_index: &Vec<u64>, adj_matrix: &Vec<Vec<u32>>) -> u64 {
+fn bubble_sort(page: &[i64], adjacency_matrix: &[Vec<i64>], sorted_pages: &[i64]) -> i64 {
+    let mut page_copy = page.to_owned();
+    let mut index = 0;
     let n = page.len();
 
-    let mut sorted_page = Vec::new();
-    sorted_page.push(page[0]);
+    loop {
+        if index == n - 1 {
+            break;
+        }
 
-    for i in 1..n {
-        sorted_page.push(page[i]);
+        let before = sorted_pages
+            .iter()
+            .position(|n| *n == page_copy[index])
+            .unwrap();
+        let after = sorted_pages
+            .iter()
+            .position(|n| *n == page_copy[index + 1])
+            .unwrap();
 
-        let mut current_index = i;
-
-        loop {
-            let current = page_index
-                .iter()
-                .position(|n| *n == sorted_page[current_index])
-                .unwrap() as usize;
-            let previous = page_index
-                .iter()
-                .position(|n| *n == sorted_page[current_index - 1])
-                .unwrap() as usize;
-
-            if adj_matrix[previous][current] == 1 {
-                break;
-            } else {
-                sorted_page.swap(current_index, current_index - 1);
-
-                if current_index == 1 {
-                    break;
-                } else {
-                    current_index -= 1;
-                }
-            }
+        if adjacency_matrix[before][after] == 1 {
+            index += 1;
+        } else {
+            page_copy.swap(index, index + 1);
+            index = index.saturating_sub(1);
         }
     }
 
-    sorted_page[n / 2]
+    page_copy[n / 2]
+}
+
+fn get_middle_page(order_rules: &[Vec<i64>], pages: &[Vec<i64>], part: bool) -> i64 {
+    let (adjacency_matrix, sorted_pages) = build_adjacency_matrix(order_rules);
+
+    pages
+        .iter()
+        .map(
+            |page| match is_page_ordered(page, &adjacency_matrix, &sorted_pages) {
+                Some(middle_value) => {
+                    if part {
+                        0
+                    } else {
+                        middle_value
+                    }
+                }
+                None => {
+                    if part {
+                        bubble_sort(page, &adjacency_matrix, &sorted_pages)
+                    } else {
+                        0
+                    }
+                }
+            },
+        )
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let (order_rules, pages) = parse_input("input/day05-test.txt");
+        assert_eq!(get_middle_page(&order_rules, &pages, false), 143);
+    }
+
+    #[test]
+    fn part_two_example() {
+        let (order_rules, pages) = parse_input("input/day05-test.txt");
+        assert_eq!(get_middle_page(&order_rules, &pages, true), 123);
+    }
 }
