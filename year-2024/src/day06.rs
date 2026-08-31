7917a02aeeceef7e3961b6f058b2e6683b0d435c
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-08-31 21:28:44 +0200

    refactored and added tests.

--- year-2024/src/day06.rs
+++ year-2024/src/day06.rs
@@ -1,357 +1,253 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
-
-use std::cmp::{max, min};
-use std::collections::HashSet;
-
 use rayon::prelude::*;
+use std::cmp::{max, min};
+use std::collections::{HashMap, HashSet};
+use std::path::Path;
+use std::sync::Arc;
 
-pub fn main() -> Result<(), Error> {
-    // let path = "input/day06-test.txt";
-    let path = "input/day06.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut guard_position: Option<CurrentPosition> = None;
-
-    let mut locations_by_row = Vec::new();
-    let mut locations_by_col = Vec::new();
-
-    let mut row = 0;
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-
-        let n = line_ok.len();
-
-        if row == 0 {
-            for _ in 0..n {
-                locations_by_col.push(Vec::new());
-            }
-        }
-
-        let guard: Vec<i32> = line_ok
-            .trim()
-            .chars()
-            .enumerate()
-            .filter(|(_, c)| *c == '^')
-            .map(|(s, _)| s as i32)
-            .collect();
-
-        if !guard.is_empty() {
-            let position = CurrentPosition {
-                x: guard[0],
-                y: row,
-                dx: 0,
-                dy: -1,
-            };
-            guard_position = Some(position);
-        }
-
-        let obstacles: Vec<i32> = line_ok
-            .trim()
-            .chars()
-            .enumerate()
-            .filter(|(_, c)| *c == '#')
-            .map(|(s, _)| s as i32)
-            .collect();
-
-        for obstacle in &obstacles {
-            locations_by_col[*obstacle as usize].push(row);
-        }
-
-        locations_by_row.push(obstacles);
-
-        row += 1;
-    }
-
-    let starting_pos = guard_position.unwrap();
-
-    let (part_one, visited) = guard_positions(&starting_pos, &locations_by_row, &locations_by_col);
-    let part_two = check_loops(&starting_pos, &locations_by_row, &locations_by_col, visited);
+pub fn main() {
+    let (rows, cols, guard, size) = parse_input("input/day06.txt");
+    let (part_one, to_check) = get_visited_locations(guard, &rows, &cols, size);
+    let part_two = get_obstructions(guard, &rows, &cols, &to_check);
 
     println!("--- Day 6: Guard Gallivant ---");
     println!(" - Part one solution: {}", part_one);
-    println!(" - Part two solution: {}", part_two);
-    println!("");
-
-    Ok(())
+    println!(" - Part two solution: {}\n", part_two);
 }
 
-#[derive(Eq, Hash, PartialEq, Clone)]
-struct CurrentPosition {
-    x: i32,
-    y: i32,
-    dx: i32,
-    dy: i32,
+type Grid = HashMap<i64, Arc<Vec<i64>>>;
+
+fn parse_input<P: AsRef<Path>>(filename: P) -> (Grid, Grid, (i64, i64), i64) {
+    let mut rows: Grid = HashMap::new();
+    let mut cols_build: HashMap<i64, Vec<i64>> = HashMap::new();
+    let mut guard_pos = None;
+    let mut size = None;
+
+    if let Ok(lines) = crate::read_lines(filename) {
+        for (row, line) in lines.map_while(Result::ok).enumerate() {
+            size = Some(line.trim().len() as i64);
+            let obstacles: Vec<i64> = line
+                .trim()
+                .chars()
+                .enumerate()
+                .filter_map(|(n, c)| (c == '#').then_some(n as i64))
+                .collect();
+
+            if let Some(col) = line.trim().chars().position(|c| c == '^').map(|n| n as i64) {
+                guard_pos = Some((row as i64, col));
+            }
+
+            if obstacles.is_empty() {
+                continue;
+            }
+
+            for c in &obstacles {
+                cols_build.entry(*c).or_default().push(row as i64);
+            }
+
+            rows.insert(row as i64, Arc::new(obstacles));
+        }
+    }
+
+    let cols: Grid = cols_build
+        .into_iter()
+        .map(|(k, v)| (k, Arc::new(v)))
+        .collect();
+
+    (rows, cols, guard_pos.unwrap(), size.unwrap())
 }
 
-#[derive(Eq, Hash, PartialEq)]
-struct Position {
-    x: i32,
-    y: i32,
+fn get_next_obstacle(
+    search_space: Option<&Arc<Vec<i64>>>,
+    position: i64,
+    is_increasing: bool,
+) -> Option<i64> {
+    match search_space {
+        Some(obstacles) => {
+            if is_increasing {
+                let (mut left, mut right) = (0, obstacles.len());
+
+                while left < right {
+                    let mid = left + (right - left) / 2;
+
+                    if obstacles[mid] > position {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                if left < obstacles.len() {
+                    Some(obstacles[left] - 1)
+                } else {
+                    None
+                }
+            } else {
+                let (mut left, mut right) = (0, obstacles.len());
+
+                while left < right {
+                    let mid = left + (right - left) / 2;
+                    if obstacles[mid] < position {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+
+                if left > 0 {
+                    Some(obstacles[left - 1] + 1)
+                } else {
+                    None
+                }
+            }
+        }
+        None => None,
+    }
 }
 
-fn guard_positions(
-    starting_pos: &CurrentPosition,
-    obstacles_row: &[Vec<i32>],
-    obstacles_col: &[Vec<i32>],
-) -> (usize, HashSet<Position>) {
-    let mut visited: HashSet<Position> = HashSet::new();
-    let mut current_pos = starting_pos.clone();
-
-    let n = obstacles_row.len();
+fn get_visited_locations(
+    start: (i64, i64),
+    rows: &Grid,
+    cols: &Grid,
+    grid_size: i64,
+) -> (usize, HashSet<(i64, i64)>) {
+    let mut visited_locations = HashSet::new();
+    let mut location = start;
+    let mut direction = (-1, 0);
 
     loop {
-        let x = current_pos.x;
-        let y = current_pos.y;
-        let dx = current_pos.dx;
-        let dy = current_pos.dy;
-
+        let (x, y) = location;
+        let (dx, dy) = direction;
         let dir = if dx != 0 { dx } else { dy };
-        let position = if dir == dx { x } else { y };
-
-        let search_space = if dir == dx {
-            &obstacles_row[y as usize]
+        let (pos, search) = if dx == dir {
+            (x, cols.get(&y))
         } else {
-            &obstacles_col[x as usize]
+            (y, rows.get(&x))
         };
 
-        if let Some(next_pos) = find_next_obstacle(search_space, position, dir) {
-            let min_pos = min(position, next_pos);
-            let max_pos = max(position, next_pos);
+        if let Some(next) = get_next_obstacle(search, pos, dir > 0) {
+            let min = min(pos, next);
+            let max = max(pos, next);
 
-            for i in min_pos..=max_pos {
-                let to_add = if dir == dx {
-                    Position { x: i as i32, y: y }
+            for i in min..=max {
+                if dir == dx {
+                    visited_locations.insert((i, y));
                 } else {
-                    Position { x: x, y: i as i32 }
-                };
-                visited.insert(to_add);
+                    visited_locations.insert((x, i));
+                }
             }
 
-            let new_dir = next_direction(dx, dy);
-
-            current_pos = if dir == dx {
-                CurrentPosition {
-                    x: next_pos,
-                    y: y,
-                    dx: new_dir.0,
-                    dy: new_dir.1,
-                }
-            } else {
-                CurrentPosition {
-                    x: x,
-                    y: next_pos,
-                    dx: new_dir.0,
-                    dy: new_dir.1,
-                }
-            };
+            location = if dir == dx { (next, y) } else { (x, next) };
+            direction = (direction.1, -direction.0);
         } else {
             if dir > 0 {
-                for i in position as usize..n {
-                    let to_add = if dir == dx {
-                        Position { x: i as i32, y: y }
+                for i in pos..grid_size {
+                    if dir == dx {
+                        visited_locations.insert((i, y));
                     } else {
-                        Position { x: x, y: i as i32 }
-                    };
-                    visited.insert(to_add);
+                        visited_locations.insert((x, i));
+                    }
                 }
             } else {
-                for i in 0..position as usize {
-                    let to_add = if dir == dx {
-                        Position { x: i as i32, y: y }
+                for i in 0..=pos {
+                    if dir == dx {
+                        visited_locations.insert((i, y));
                     } else {
-                        Position { x: x, y: i as i32 }
-                    };
-                    visited.insert(to_add);
+                        visited_locations.insert((x, i));
+                    }
                 }
             }
-
             break;
         }
     }
 
-    (visited.len(), visited)
+    (visited_locations.len(), visited_locations)
 }
 
-fn find_next_obstacle(search_space: &Vec<i32>, pos: i32, dir: i32) -> Option<i32> {
-    if dir > 0 {
-        let obstacles: Vec<&i32> = search_space.iter().filter(|x| **x > pos).collect();
-
-        if !obstacles.is_empty() {
-            return Some(*obstacles[0] - 1);
-        }
-    } else {
-        let obstacles: Vec<&i32> = search_space.iter().filter(|x| **x < pos).collect();
-
-        if !obstacles.is_empty() {
-            let n = obstacles.len();
-            return Some(*obstacles[n - 1] + 1);
-        }
-    }
-
-    None
-}
-
-fn next_direction(dx: i32, dy: i32) -> (i32, i32) {
-    let next_position = match (dx, dy) {
-        (1, 0) => (0, 1),
-        (0, 1) => (-1, 0),
-        (-1, 0) => (0, -1),
-        (0, -1) => (1, 0),
-        _ => (0, 0),
-    };
-
-    next_position
-}
-
-fn check_loops(
-    starting_pos: &CurrentPosition,
-    obstacles_row: &[Vec<i32>],
-    obstacles_col: &[Vec<i32>],
-    to_check: HashSet<Position>,
-) -> i32 {
-    to_check
-        .par_iter()
-        .map(|position| {
-            let x = position.x as usize;
-            let y = position.y as usize;
-            let mut temp_row = obstacles_row.to_vec();
-            let mut temp_col = obstacles_col.to_vec();
-
-            let row_pos = temp_row[y]
-                .binary_search(&(position.x))
-                .unwrap_or_else(|e| e);
-            temp_row[y].insert(row_pos, position.x);
-
-            let col_pos = temp_col[x]
-                .binary_search(&(position.y))
-                .unwrap_or_else(|e| e);
-            temp_col[x].insert(col_pos, position.y);
-
-            loop_guard(starting_pos, &temp_row, &temp_col) as i32
-        })
-        .sum()
-}
-
-fn loop_guard(
-    starting_pos: &CurrentPosition,
-    obstacles_row: &[Vec<i32>],
-    obstacles_col: &[Vec<i32>],
-) -> bool {
-    let mut visited: HashSet<CurrentPosition> = HashSet::new();
-    let mut current_pos = starting_pos.clone();
-
-    let n = obstacles_row.len();
+fn is_looping(start: (i64, i64), rows: &Grid, cols: &Grid) -> bool {
+    let mut visited_locations = HashSet::new();
+    let mut location = start;
+    let mut direction = (-1, 0);
 
     loop {
-        if visited.contains(&current_pos) {
+        let (x, y) = location;
+        let (dx, dy) = direction;
+
+        if visited_locations.contains(&(x, y, dx, dy)) {
             return true;
         }
 
-        let x = current_pos.x;
-        let y = current_pos.y;
-        let dx = current_pos.dx;
-        let dy = current_pos.dy;
-
         let dir = if dx != 0 { dx } else { dy };
-        let position = if dir == dx { x } else { y };
-
-        let search_space = if dir == dx {
-            &obstacles_row[y as usize]
+        let (pos, search) = if dx == dir {
+            (x, cols.get(&y))
         } else {
-            &obstacles_col[x as usize]
+            (y, rows.get(&x))
         };
 
-        if let Some(next_pos) = find_next_obstacle(search_space, position, dir) {
-            let min_pos = min(position, next_pos);
-            let max_pos = max(position, next_pos);
+        if let Some(next) = get_next_obstacle(search, pos, dir > 0) {
+            let min = min(pos, next);
+            let max = max(pos, next);
 
-            for i in min_pos..=max_pos {
-                let to_add = if dir == dx {
-                    CurrentPosition {
-                        x: i as i32,
-                        y: y,
-                        dx: dx,
-                        dy: dy,
-                    }
+            for i in min..=max {
+                if dir == dx {
+                    visited_locations.insert((i, y, dx, dy));
                 } else {
-                    CurrentPosition {
-                        x: x,
-                        y: i as i32,
-                        dx: dx,
-                        dy: dy,
-                    }
-                };
-
-                visited.insert(to_add);
+                    visited_locations.insert((x, i, dx, dy));
+                }
             }
 
-            let new_dir = next_direction(dx, dy);
-
-            current_pos = if dir == dx {
-                CurrentPosition {
-                    x: next_pos,
-                    y: y,
-                    dx: new_dir.0,
-                    dy: new_dir.1,
-                }
-            } else {
-                CurrentPosition {
-                    x: x,
-                    y: next_pos,
-                    dx: new_dir.0,
-                    dy: new_dir.1,
-                }
-            };
+            location = if dir == dx { (next, y) } else { (x, next) };
+            direction = (direction.1, -direction.0);
         } else {
-            if dir > 0 {
-                for i in position as usize..n {
-                    let to_add = if dir == dx {
-                        CurrentPosition {
-                            x: i as i32,
-                            y: y,
-                            dx: dx,
-                            dy: dy,
-                        }
-                    } else {
-                        CurrentPosition {
-                            x: x,
-                            y: i as i32,
-                            dx: dx,
-                            dy: dy,
-                        }
-                    };
-
-                    visited.insert(to_add);
-                }
-            } else {
-                for i in 0..position as usize {
-                    let to_add = if dir == dx {
-                        CurrentPosition {
-                            x: i as i32,
-                            y: y,
-                            dx: dx,
-                            dy: dy,
-                        }
-                    } else {
-                        CurrentPosition {
-                            x: x,
-                            y: i as i32,
-                            dx: dx,
-                            dy: dy,
-                        }
-                    };
-
-                    visited.insert(to_add);
-                }
-            }
-
             break;
         }
     }
 
     false
+}
+
+fn get_obstructions(
+    start: (i64, i64),
+    rows: &Grid,
+    cols: &Grid,
+    to_check: &HashSet<(i64, i64)>,
+) -> i64 {
+    to_check
+        .par_iter()
+        .map(|(x, y)| {
+            let mut new_rows = rows.clone();
+            let mut new_cols = cols.clone();
+
+            let row = new_rows.entry(*x).or_insert_with(|| Arc::new(Vec::new()));
+            let mut row_vec = (**row).clone();
+            let r = row_vec.binary_search(y).unwrap_or_else(|n| n);
+            row_vec.insert(r, *y);
+            *row = Arc::new(row_vec);
+
+            let col = new_cols.entry(*y).or_insert_with(|| Arc::new(Vec::new()));
+            let mut col_vec = (**col).clone();
+            let c = col_vec.binary_search(x).unwrap_or_else(|n| n);
+            col_vec.insert(c, *x);
+            *col = Arc::new(col_vec);
+
+            is_looping(start, &new_rows, &new_cols) as i64
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let (rows, cols, guard, size) = parse_input("input/day06-test.txt");
+        let (visited_len, _) = get_visited_locations(guard, &rows, &cols, size);
+        assert_eq!(visited_len, 41);
+    }
+
+    #[test]
+    fn part_two_example() {
+        let (rows, cols, guard, size) = parse_input("input/day06-test.txt");
+        let (_, to_check) = get_visited_locations(guard, &rows, &cols, size);
+        assert_eq!(get_obstructions(guard, &rows, &cols, &to_check), 6);
+    }
 }
