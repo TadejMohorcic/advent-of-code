dab202ed161a8697a8e63c38ef7a132a0f5edab1
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-09-09 18:23:59 +0200

    update to xgcd

--- year-2024/src/day14.rs
+++ year-2024/src/day14.rs
@@ -80,15 +80,9 @@ fn xgcd(a: i64, b: i64) -> (i64, i64) {
 
     while r1 != 0 {
         let q = r0 / r1;
-        let r = r0 - q * r1;
-        let s = s0 - q * s1;
-        let t = t0 - q * t1;
-        r0 = r1;
-        r1 = r;
-        s0 = s1;
-        s1 = s;
-        t0 = t1;
-        t1 = t;
+        (r0, r1) = (r1, r0 - q * r1);
+        (s0, s1) = (s1, s0 - q * s1);
+        (t0, t1) = (t1, t0 - q * t1);
     }
 
     (s0, t0)
