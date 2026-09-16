86fc1e9a14d24722b8a130d00b99eebfc7c71ebd
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2026-09-16 20:41:18 +0200

    Refactor

--- year-2024/src/day17.rs
+++ year-2024/src/day17.rs
@@ -1,87 +1,73 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::io;
+use std::path::Path;
 
-pub fn main() -> Result<(), Error> {
-    // let path = "input/day17-test.txt";
-    let path = "input/day17.txt";
-
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let mut index = 0;
-    let mut register = [0; 3];
-
-    let mut instructions: Option<Vec<usize>> = None;
-
-    for line in buffered.lines() {
-        let line_ok = line?;
-
-        if index > 2 {
-            if !line_ok.is_empty() {
-                instructions = Some(
-                    line_ok
-                        .trim()
-                        .split_once(": ")
-                        .unwrap()
-                        .1
-                        .split(',')
-                        .map(|x| x.parse().expect("Should be a number!"))
-                        .collect(),
-                );
-            }
-        } else {
-            let value: i64 = line_ok
-                .trim()
-                .split_once(": ")
-                .unwrap()
-                .1
-                .parse()
-                .expect("Should be a number!");
-
-            register[index] = value;
-            index += 1;
-        }
-    }
-
-    let instructions = instructions.unwrap();
-
-    let part_one = run_program(&mut register, &instructions);
-    let part_two = find_starting_value(0, &instructions, 0);
+pub fn main() -> io::Result<()> {
+    let (mut register, program) = parse_input("input/day17.txt")?;
+    let part_one = run_program(&mut register, &program);
+    let part_two = find_starting_value(&program, 0, 0);
 
     println!("--- Day 17: Chronospatial Computer ---");
     println!(" - Part one solution: {:?}", part_one);
-    println!(" - Part two solution: {}", part_two.iter().min().unwrap());
-    println!("");
+    println!(" - Part two solution: {}\n", part_two.iter().min().unwrap());
 
     Ok(())
 }
 
-fn run_program(register: &mut [i64; 3], instructions: &Vec<usize>) -> Vec<usize> {
-    let mut pointer = 0;
-    let n = instructions.len();
+fn parse_input<P: AsRef<Path>>(filename: P) -> io::Result<([i64; 3], Vec<i64>)> {
+    let lines = crate::read_lines(&filename)?;
+    let mut register = [0; 3];
+    let mut register_counter = 0;
+    let mut program = None;
 
-    let mut out = Vec::new();
+    for line in lines.map_while(Result::ok) {
+        if line.is_empty() {
+            continue;
+        }
+
+        if register_counter < 3 {
+            register[register_counter] = line
+                .trim()
+                .split(": ")
+                .last()
+                .unwrap()
+                .parse::<i64>()
+                .unwrap();
+            register_counter += 1;
+        } else {
+            let p = line.trim().split(": ").last().unwrap();
+            program = Some(p.split(',').map(|n| n.parse::<i64>().unwrap()).collect());
+        }
+    }
+
+    Ok((register, program.unwrap()))
+}
+
+fn run_program(register: &mut [i64; 3], program: &[i64]) -> String {
+    let mut pointer = 0;
+    let n = program.len();
+    let mut output: Vec<i64> = Vec::new();
 
     loop {
         if pointer >= n {
-            return out;
+            return output
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(",");
         }
 
-        let instruction = instructions[pointer];
-        let literal = instructions[pointer + 1] as i64;
+        let mut jump = false;
+        let instruction = program[pointer];
+        let literal = program[pointer + 1];
         let combo = match literal {
             0..=3 => literal,
             4 => register[0],
             5 => register[1],
             6 => register[2],
-            7 => 7,
-            _ => unreachable!("Should not appear anywhere!"),
+            _ => 7,
         };
-
-        let mut jmp = false;
-
         match instruction {
-            0 => register[0] = register[0] / 2_i64.pow(combo as u32),
+            0 => register[0] /= 2_i64.pow(combo as u32),
             1 => {
                 let b = register[1];
                 register[1] = b ^ literal;
@@ -90,7 +76,7 @@ fn run_program(register: &mut [i64; 3], instructions: &Vec<usize>) -> Vec<usize>
             3 => {
                 if register[0] != 0 {
                     pointer = literal as usize;
-                    jmp = true;
+                    jump = true;
                 }
             }
             4 => {
@@ -98,26 +84,26 @@ fn run_program(register: &mut [i64; 3], instructions: &Vec<usize>) -> Vec<usize>
                 let c = register[2];
                 register[1] = b ^ c;
             }
-            5 => out.push(combo as usize % 8),
+            5 => output.push(combo % 8),
             6 => register[1] = register[0] / 2_i64.pow(combo as u32),
-            7 => register[2] = register[0] / 2_i64.pow(combo as u32),
-            _ => unreachable!("Invalid instruction!"),
+            _ => register[2] = register[0] / 2_i64.pow(combo as u32),
         }
 
-        if !jmp {
+        if !jump {
             pointer += 2;
         }
     }
 }
 
-fn find_starting_value(mut a: i64, instructions: &Vec<usize>, index: usize) -> Vec<i64> {
-    let n = instructions.len();
+// Reddit solution
+fn find_starting_value(program: &[i64], mut a: i64, index: usize) -> Vec<i64> {
+    let n = program.len();
 
     if index == n {
         return vec![a];
     }
 
-    a = a << 3;
+    a <<= 3;
     let mut solutions = Vec::new();
 
     for i in 0..8 {
@@ -125,10 +111,21 @@ fn find_starting_value(mut a: i64, instructions: &Vec<usize>, index: usize) -> V
         let mut b = (new_a % 8) ^ 3;
         b = (b ^ (new_a >> b) ^ 5) % 8;
 
-        if b as usize == instructions[n - 1 - index] {
-            solutions.extend(find_starting_value(new_a, instructions, index + 1));
+        if b == program[n - 1 - index] {
+            solutions.extend(find_starting_value(program, new_a, index + 1));
         }
     }
 
     solutions
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let (mut register, program) = parse_input("input/day17-test.txt").unwrap();
+        assert_eq!(run_program(&mut register, &program), "4,6,3,5,6,3,5,2,1,0");
+    }
 }
